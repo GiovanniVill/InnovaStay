3d310926c705ca3052206ce6da8ad88a9f0299b4
Rhyen Miguel Gutierrez Peñamayor <rhyenmiguelg@gmail.com>
2026-09-18 14:49:10 +0800

    US-01: Add AdminDashboardForm and FrontDeskDashboardForm layout designs

---
+++ InnovaStaySolution/UI/AdminDashboardForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace UI
+{
+    partial class AdminDashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            lblSum = new Label();
+            lblTotal = new Label();
+            lblAvail = new Label();
+            lblOccu = new Label();
+            lblRes = new Label();
+            lblQuick = new Label();
+            btnRM = new Button();
+            btnUM = new Button();
+            SuspendLayout();
+            // 
+            // lblSum
+            // 
+            lblSum.AutoSize = true;
+            lblSum.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblSum.Location = new Point(21, 22);
+            lblSum.Name = "lblSum";
+            lblSum.Size = new Size(161, 39);
+            lblSum.TabIndex = 0;
+            lblSum.Text = "Summaries:";
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Font = new Font("Sitka Heading", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTotal.Location = new Point(21, 67);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(148, 35);
+            lblTotal.TabIndex = 1;
+            lblTotal.Text = "Total Rooms";
+            // 
+            // lblAvail
+            // 
+            lblAvail.AutoSize = true;
+            lblAvail.Font = new Font("Sitka Heading", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblAvail.Location = new Point(236, 67);
+            lblAvail.Name = "lblAvail";
+            lblAvail.Size = new Size(111, 35);
+            lblAvail.TabIndex = 2;
+            lblAvail.Text = "Available";
+            // 
+            // lblOccu
+            // 
+            lblOccu.AutoSize = true;
+            lblOccu.Font = new Font("Sitka Heading", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblOccu.Location = new Point(424, 67);
+            lblOccu.Name = "lblOccu";
+            lblOccu.Size = new Size(110, 35);
+            lblOccu.TabIndex = 3;
+            lblOccu.Text = "Occupied";
+            // 
+            // lblRes
+            // 
+            lblRes.AutoSize = true;
+            lblRes.Font = new Font("Sitka Heading", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblRes.Location = new Point(612, 67);
+            lblRes.Name = "lblRes";
+            lblRes.Size = new Size(154, 35);
+            lblRes.TabIndex = 4;
+            lblRes.Text = "Reservations";
+            // 
+            // lblQuick
+            // 
+            lblQuick.AutoSize = true;
+            lblQuick.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblQuick.Location = new Point(21, 266);
+            lblQuick.Name = "lblQuick";
+            lblQuick.Size = new Size(167, 39);
+            lblQuick.TabIndex = 5;
+            lblQuick.Text = "Quick Links:";
+            // 
+            // btnRM
+            // 
+            btnRM.BackColor = Color.Khaki;
+            btnRM.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRM.Location = new Point(89, 319);
+            btnRM.Name = "btnRM";
+            btnRM.Size = new Size(307, 63);
+            btnRM.TabIndex = 6;
+            btnRM.Text = "Room Maintenance";
+            btnRM.UseVisualStyleBackColor = false;
+            // 
+            // btnUM
+            // 
+            btnUM.BackColor = Color.Khaki;
+            btnUM.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnUM.Location = new Point(414, 319);
+            btnUM.Name = "btnUM";
+            btnUM.Size = new Size(306, 63);
+            btnUM.TabIndex = 7;
+            btnUM.Text = "User Management";
+            btnUM.UseVisualStyleBackColor = false;
+            // 
+            // AdminDashboardForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Honeydew;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnUM);
+            Controls.Add(btnRM);
+            Controls.Add(lblQuick);
+            Controls.Add(lblRes);
+            Controls.Add(lblOccu);
+            Controls.Add(lblAvail);
+            Controls.Add(lblTotal);
+            Controls.Add(lblSum);
+            Name = "AdminDashboardForm";
+            Text = "AdminDashboardForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private Label lblSum;
+        private Label lblTotal;
+        private Label lblAvail;
+        private Label lblOccu;
+        private Label lblRes;
+        private Label lblQuick;
+        private Button btnRM;
+        private Button btnUM;
+    }
+}
---
+++ InnovaStaySolution/UI/FrontDeskDashboardForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace UI
+{
+    partial class FrontDeskDashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCI = new Button();
+            btnCO = new Button();
+            btnAvailR = new Button();
+            btnActiveR = new Button();
+            label1 = new Label();
+            btnCheck = new Button();
+            btnOut = new Button();
+            lblQuickActs = new Label();
+            btnSearchAvail = new Button();
+            btnCreateRes = new Button();
+            btnBP = new Button();
+            SuspendLayout();
+            // 
+            // btnCI
+            // 
+            btnCI.BackColor = Color.Khaki;
+            btnCI.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCI.Location = new Point(45, 46);
+            btnCI.Name = "btnCI";
+            btnCI.Size = new Size(271, 51);
+            btnCI.TabIndex = 0;
+            btnCI.Text = "Today's Check-in";
+            btnCI.UseVisualStyleBackColor = false;
+            // 
+            // btnCO
+            // 
+            btnCO.BackColor = Color.Khaki;
+            btnCO.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCO.Location = new Point(45, 123);
+            btnCO.Name = "btnCO";
+            btnCO.Size = new Size(271, 51);
+            btnCO.TabIndex = 1;
+            btnCO.Text = "Today's Check-out";
+            btnCO.UseVisualStyleBackColor = false;
+            // 
+            // btnAvailR
+            // 
+            btnAvailR.BackColor = Color.Khaki;
+            btnAvailR.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAvailR.Location = new Point(45, 198);
+            btnAvailR.Name = "btnAvailR";
+            btnAvailR.Size = new Size(271, 51);
+            btnAvailR.TabIndex = 2;
+            btnAvailR.Text = "Available Rooms";
+            btnAvailR.UseVisualStyleBackColor = false;
+            // 
+            // btnActiveR
+            // 
+            btnActiveR.BackColor = Color.Khaki;
+            btnActiveR.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnActiveR.Location = new Point(45, 273);
+            btnActiveR.Name = "btnActiveR";
+            btnActiveR.Size = new Size(271, 51);
+            btnActiveR.TabIndex = 3;
+            btnActiveR.Text = "Active Reservations";
+            btnActiveR.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(37, 327);
+            label1.Name = "label1";
+            label1.Size = new Size(338, 17);
+            label1.TabIndex = 4;
+            label1.Text = "__________________________________________________________________";
+            // 
+            // btnCheck
+            // 
+            btnCheck.BackColor = Color.Khaki;
+            btnCheck.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCheck.Location = new Point(37, 364);
+            btnCheck.Name = "btnCheck";
+            btnCheck.Size = new Size(139, 51);
+            btnCheck.TabIndex = 5;
+            btnCheck.Text = "Check-In";
+            btnCheck.UseVisualStyleBackColor = false;
+            // 
+            // btnOut
+            // 
+            btnOut.BackColor = Color.Khaki;
+            btnOut.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnOut.Location = new Point(182, 364);
+            btnOut.Name = "btnOut";
+            btnOut.Size = new Size(150, 51);
+            btnOut.TabIndex = 6;
+            btnOut.Text = "Check-Out";
+            btnOut.UseVisualStyleBackColor = false;
+            // 
+            // lblQuickActs
+            // 
+            lblQuickActs.AutoSize = true;
+            lblQuickActs.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblQuickActs.Location = new Point(372, 9);
+            lblQuickActs.Name = "lblQuickActs";
+            lblQuickActs.Size = new Size(190, 39);
+            lblQuickActs.TabIndex = 7;
+            lblQuickActs.Text = "Quick Actions:";
+            // 
+            // btnSearchAvail
+            // 
+            btnSearchAvail.BackColor = Color.Khaki;
+            btnSearchAvail.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSearchAvail.Location = new Point(434, 81);
+            btnSearchAvail.Name = "btnSearchAvail";
+            btnSearchAvail.Size = new Size(258, 51);
+            btnSearchAvail.TabIndex = 8;
+            btnSearchAvail.Text = "Search Availability";
+            btnSearchAvail.UseVisualStyleBackColor = false;
+            // 
+            // btnCreateRes
+            // 
+            btnCreateRes.BackColor = Color.Khaki;
+            btnCreateRes.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCreateRes.Location = new Point(434, 163);
+            btnCreateRes.Name = "btnCreateRes";
+            btnCreateRes.Size = new Size(258, 51);
+            btnCreateRes.TabIndex = 9;
+            btnCreateRes.Text = "Create Reservation";
+            btnCreateRes.UseVisualStyleBackColor = false;
+            // 
+            // btnBP
+            // 
+            btnBP.BackColor = Color.Khaki;
+            btnBP.Font = new Font("Sitka Heading", 20.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBP.Location = new Point(453, 241);
+            btnBP.Name = "btnBP";
+            btnBP.Size = new Size(223, 51);
+            btnBP.TabIndex = 10;
+            btnBP.Text = "Billing/Payment";
+            btnBP.UseVisualStyleBackColor = false;
+            // 
+            // FrontDeskDashboardForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Honeydew;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnBP);
+            Controls.Add(btnCreateRes);
+            Controls.Add(btnSearchAvail);
+            Controls.Add(lblQuickActs);
+            Controls.Add(btnOut);
+            Controls.Add(btnCheck);
+            Controls.Add(label1);
+            Controls.Add(btnActiveR);
+            Controls.Add(btnAvailR);
+            Controls.Add(btnCO);
+            Controls.Add(btnCI);
+            Name = "FrontDeskDashboardForm";
+            Text = "FrontDeskDashboardForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnCI;
+        private Button btnCO;
+        private Button btnAvailR;
+        private Button btnActiveR;
+        private Label label1;
+        private Button btnCheck;
+        private Button btnOut;
+        private Label lblQuickActs;
+        private Button btnSearchAvail;
+        private Button btnCreateRes;
+        private Button btnBP;
+    }
+}
